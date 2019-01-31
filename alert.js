337249538d32a921c7d53637f84ea73be1372552
Askhat <34422136+hiaskhat@users.noreply.github.com>
2019-01-31 18:41:43 +0500

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,10 @@
-alert('Learn JavaScript')
+//alert('Learn JavaScript');
+
+var admin;
+var name = 'Vasiliy';
+
+admin = name;
+alert( admin );
+
+//Также переменные можем создавать через запятую
+//var admin, name;
